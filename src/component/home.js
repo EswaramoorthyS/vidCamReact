c6ec4ac63917357557b7782e13938a4f485a26a5
EswaramoorthyS <smeswaramoorthy@gmail.com>
2023-10-08 13:48:30 +0530

    initial commit

---
+++ src/component/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Webcam from "react-webcam";
+const Home = () => {
+
+    const tocuh = () => {
+        alert('You are hacked ..!!')
+    }
+
+    return (
+
+        <Webcam
+            onClick={tocuh}
+            audio={false}
+            height={'10%'}
+            screenshotFormat="image/jpeg"
+            width={'100%'}
+        >
+        </Webcam>
+
+    );
+};
+
+export default Home;
